17a2c77a13a4c141ffc5e88cee456baa0f949c01
PauloIletski <paulogarcia-vieira@outlook.com>
2025-05-27 14:49:38 -0300

    feat: warning about instagram

--- pages/issacar-galeries/[slug]/index.tsx
+++ pages/issacar-galeries/[slug]/index.tsx
@@ -66,14 +66,14 @@ const GalleryPage: NextPage<Props> = ({ images, slug }) => {
             <p className="max-w-[40ch] text-white/75 sm:max-w-[32ch]">
               <strong>Atenção:</strong> Para conseguir baixar as fotos do culto, <strong>utilize o navegador de seu celular</strong>
             </p>
-            <a
+            {/* <a
               className="pointer z-10 mt-6 rounded-lg border border-white bg-white px-3 py-2 text-sm font-semibold text-black transition hover:bg-white/10 hover:text-white md:mt-4"
               href="https://wa.me/5511941234567?text=Oi%,%20quero%20saber%20mais%20sobre%20o%20aplicativo%20de%20fotos%20e%20fazer%20sugestões!"
               target="_blank"
               rel="noreferrer"
             >
               Sugerir uma melhoria.
-            </a>
+            </a> */}
           </div>
           {images.map(({ id, public_id, format, blurDataUrl, height, width }) => (
             <Link
