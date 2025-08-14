06c11da8a3ad816c8a34d9b8f6c1a232102c30e5
PauloIletski <paulogarcia-vieira@outlook.com>
2025-08-14 14:18:52 -0300

    fix: adjust facebook page link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -32,7 +32,7 @@ export default function Footer() {
     },
     {
       name: 'Facebook',
-      url: 'https://www.facebook.com/issacar.church',
+        url: 'https://www.facebook.com/adissacar',
       icon: FaFacebook,
       color: 'bg-blue-600 hover:bg-blue-700'
     }
